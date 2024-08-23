c69ea0878100c1a5d523b3b08d99a69c323ad1c0
Michael Weigle <michael.weigle@gmail.com>
2024-08-22 23:49:10 -0400

    first commit

---
+++ src/main/java/com/metricmike/example_vscodegradle_1589/ExampleVscodegradle1589Application.java
@@ -0,0 +1,13 @@
+package com.metricmike.example_vscodegradle_1589;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleVscodegradle1589Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleVscodegradle1589Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/metricmike/example_vscodegradle_1589/ExampleVscodegradle1589ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metricmike.example_vscodegradle_1589;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleVscodegradle1589ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
